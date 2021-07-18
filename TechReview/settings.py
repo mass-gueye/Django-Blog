eb044a1005e084db3f49625a93c7c6100574c208
mass-gueye <80859094+mass-gueye@users.noreply.github.com>
2021-07-18 19:10:53 +0000

    Update settings.py

--- TechReview/settings.py
+++ TechReview/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@#a2_)dtqmt7cpix20$-o8@3zxycpojrm5!brbd^v7*vc#)k5)'
+SECRET_KEY = 'yoursecretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
